0f36835a965488345eb10c600b1f89a881dbd608
Cameron Morrison <cameron.morrison@dingublue.com>
2014-02-10 14:39:39 +1000

    tweaks

--- profile.ps1
+++ profile.ps1
@@ -23,7 +23,7 @@ new-alias -force which get-command
 new-alias -force help get-help
 new-alias -force gh get-help
 
-Set-Location c:\
+# Set-Location c:\
 
 function prompt(){ 
 	#colours
@@ -130,4 +130,20 @@ function Invoke-SshAgent([switch]$Quiet) {
     Start-SshAgent($Quiet);
 
     $env:path = $oldPath;
+}
+
+function Invoke-WMSettingChange {
+    #requires -version 2
+
+    if (-not ("win32.nativemethods" -as [type])) {
+        # import sendmessagetimeout from win32
+        add-type -Namespace Win32 -Name NativeMethods -MemberDefinition '[DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)] public static extern IntPtr SendMessageTimeout(IntPtr hWnd, uint Msg, UIntPtr wParam, string lParam, uint fuFlags, uint uTimeout, out UIntPtr lpdwResult);'
+    }
+
+    $HWND_BROADCAST = [intptr]0xffff;
+    $WM_SETTINGCHANGE = 0x1a;
+    $result = [uintptr]::zero
+
+    # notify all windows of environment block change
+    [win32.nativemethods]::SendMessageTimeout($HWND_BROADCAST, $WM_SETTINGCHANGE, [uintptr]::Zero, "Environment", 2, 5000, [ref]$result);
 }
